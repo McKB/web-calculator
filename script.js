df5d351bdaf6f954c9b5e11bc69f1ab099c6f1b6
McKB <mckennakb@comcast.net>
2022-03-23 18:53:14 -0400

    adds subtraction, multiplication, and division options

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 
-// function addNumbers takes two values from input fields, stores them
-// in variables, adds them together and store this value in another variable. 
+// function doMath() takes two values from input fields, stores them
+// in variables. Then them together and store this value in another variable. 
 // The result is displyed on the screen, and the inputs are cleared.
-function addNumbers(){
+function doMath(){
     var num1 = Number(document.getElementById('input1').value);
     var num2 = Number(document.getElementById('input2').value);
-    var answer = num1 + num2;
-    clearAll();
-    document.getElementById('results').innerHTML =(num1 + " + " + num2 + " = " + answer);
+    var operator = document.getElementById('operation').value;
+    var result = document.getElementById('results');
+    var answer;
+    if (operator === '+') {
+        answer = num1 + num2;
+        clearAll();
+        result.innerHTML = (num1 + " + " + num2 + " = " + answer);
+    } else if (operator === '-') {
+        answer = num1 - num2;
+        clearAll();
+        result.innerHTML = (num1 + " - " + num2 + " = " + answer);
+    } else if (operator === "x") {
+        answer = num1 * num2;
+        clearAll();
+        result.innerHTML = (num1 + " x " + num2 + " = " + answer);
+    } else {
+        answer = num1 / num2;
+        clearAll();
+        result.innerHTML = (num1 + " / " + num2 + " = " + answer);
+    }
 }
 
 // clears all data from input boxes & results. clean slate. 
@@ -17,16 +34,21 @@ function clearAll(){
     document.getElementById('results').innerHTML = '';
 }
 
+// add comment here
 function chooseOperation(){
     var operator = document.getElementById('operation').value;
-    if (operator == '+') {
-        operator = '-'
-    } else if (operator == '-') {
-        operator = 'x'
-    } else if (operator == 'x') {
-        operator = '/'
+    if (operator === '+') {
+        document.getElementById('operation').value = '-';
+        operator = '-';
+    } else if (operator === '-') {
+        document.getElementById('operation').value = 'x';
+        operator = 'x';
+    } else if (operator === 'x') {
+        document.getElementById('operation').value = '/';
+        operator = '/';
     } else {
-        operator = '+'
+        document.getElementById('operation').value = '+';
+        operator = '+';
     };
     return operator;
 }
